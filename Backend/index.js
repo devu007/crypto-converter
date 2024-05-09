f47410c940227c06073d1fdad08dc6dde90c761f
Dev Tyagi <devutyagi2001@gmail.com>
2024-05-09 12:27:54 +0530

    Documentation

--- Backend/index.js
+++ Backend/index.js
@@ -1,14 +1,17 @@
-import express, { urlencoded } from "express";
+import express from "express";
 import route from "./routes/api.js";
 import corsMiddleware from "./middlewares/cors.js";
 import errorHandlerMiddleware from "./middlewares/errorHandler.js";
 
 const app = express();
 
+// Use the CORS middleware
 app.use(corsMiddleware);
 
+// Use the API routes
 app.use("/", route);
 
+// Use the error handler middleware
 app.use(errorHandlerMiddleware);
 
 const PORT = 3000;
